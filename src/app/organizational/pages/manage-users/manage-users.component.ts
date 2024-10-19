c8598b9f20e5fd5f7150d381a45252fa5e5437df
Jairo-18 <jhonlegarda1.2@gmail.com>
2024-10-19 13:16:27 -0500

    Upload: create-user

--- src/app/organizational/pages/manage-users/manage-users.component.ts
+++ src/app/organizational/pages/manage-users/manage-users.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { BasePageComponent } from "../../../shared/components/base-page/base-page.component";
+import { BaseCardComponent } from '../../../shared/components/base-card/base-card.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-users',
   standalone: true,
-  imports: [],
+  imports: [
+    BasePageComponent,
+    BaseCardComponent,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './manage-users.component.html',
   styleUrl: './manage-users.component.scss'
 })
