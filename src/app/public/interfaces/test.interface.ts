e71e836cea6221a27d5c701e97b4f5dccabceb22
Jairo-18 <jhonlegarda1.2@gmail.com>
2024-10-21 14:33:20 -0500

    Test: user-create

--- src/app/public/interfaces/test.interface.ts
+++ src/app/public/interfaces/test.interface.ts
@@ -1,11 +1,28 @@
-export interface UserInterface {
-    fullName:                 string;
-    identification:       string;
-    username:             string;
-    email:                string;
-    createdAt:            Date;
-    phone:                string;
-    avatarUrl:            string;
-    roleId:               number;
-    identificationTypeId: number;
+import { Roles } from '../../auth/interfaces/login.interface';
+
+export interface UsersInterface {
+  id: string;
+  identificationTypeId: number;
+  roleId: number;
+  identification: string;
+  fullName: string;
+  avatarUrl: string;
+  username: string;
+  phone: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+  identificationType: IdentificationType;
+  role: Roles;
+}
+
+export interface IdentificationType {
+  id: number;
+  type?: string;
+  code: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+  name?: string;
 }
--- src/app/public/services/test.service.ts
+++ src/app/public/services/test.service.ts
@@ -3,20 +3,25 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiResponseInterface } from '../../shared/interfaces/api-response.interface';
-import { UserInterface } from '../interfaces/test.interface';
 import { environment } from '../../../environments/environment.development';
+import { UsersInterface } from '../../users/interfaces/users.interface';
+import { HttpUtilitiesService } from '../../shared/utilities/http-utilities.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
   private readonly _httpClient: HttpClient = inject(HttpClient);
+  private readonly _httpUtilities: HttpUtilitiesService =
+    inject(HttpUtilitiesService);
 
-  getUserProfile(
-    userId: string
-  ): Observable<ApiResponseInterface<UserInterface>> {
-    return this._httpClient.get<ApiResponseInterface<UserInterface>>(
-      `${environment.apiUrl}user/${userId}`
+  getUserWithPagination(
+    query: object
+  ): Observable<ApiResponseInterface<UsersInterface[]>> {
+    const params = this._httpUtilities.httpParamsFromObject(query);
+    return this._httpClient.get<ApiResponseInterface<UsersInterface[]>>(
+      `${environment.apiUrl}user/paginated-list`,
+      { params }
     );
   }
 }
