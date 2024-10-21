5a58d75f0ee11acfa026d81c51f617a7f09fc338
Jheff <divegas43@gmail.com>
2024-10-21 18:11:32 -0500

    Fixed: bigs

--- src/app/public/pages/test/test.component.ts
+++ src/app/public/pages/test/test.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 // test.component.ts
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
@@ -17,12 +17,12 @@ import { BaseCardComponent } from '../../../shared/components/base-card/base-car
 import { BasePageComponent } from '../../../shared/components/base-page/base-page.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import { Router } from '@angular/router';
+// import { Router } from '@angular/router';
 import { MatSelectModule } from '@angular/material/select';
-import { v4 as uuidv4 } from 'uuid';
+//import { v4 as uuidv4 } from 'uuid';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatInputModule } from '@angular/material/input';
-import { TestService } from '../../services/test.service';
+// import { TestService } from '../../services/test.service';
 
 @Component({
   selector: 'app-test',
@@ -54,8 +54,8 @@ export class TestComponent {
   eyeClose = faEyeSlash;
   showPassword: boolean = false;
   showConfirmPassword: boolean = false;
-  private readonly _userRoleService: TestService = inject(TestService);
-  private readonly _router: Router = inject(Router);
+  // private readonly _userRoleService: TestService = inject(TestService);
+  // private readonly _router: Router = inject(Router);
 
   identificationTypes: { type: string; id: number }[] = [
     { type: 'Cédula de ciudadanía', id: 1 },
@@ -106,30 +106,30 @@ export class TestComponent {
   /**
    * Enviar formulario al empoint del backend.
    */
-  onSubmit() {
-    if (this.formRolUser.valid) {
-      const userToRegister: TestService = {
-        id: uuidv4(),
-        identification: this.formRolUser.value.identification,
-        identificationTypeId: 1,
-        fullName: this.formRolUser.value.fullName,
-        avatarUrl: this.formRolUser.value.avatarUrl,
-        username: this.formRolUser.value.username,
-        phone: this.formRolUser.value.phone,
-        email: this.formRolUser.value.email,
-        password: this.formRolUser.value.password,
-        passwordConfirmation: this.formRolUser.get('confirmPassword')?.value,
-        roleId: 1
-      };
-      this._userRoleService.rolUser(userToRegister).subscribe({
-        next: () => {
-          this._router.navigate(['/home']);
-        }
-      });
-    } else {
-      // Muestra errores si uno de los formularios no es válido
-    }
-  }
+  // onSubmit() {
+  //   if (this.formRolUser.valid) {
+  //     const userToRegister: TestService = {
+  //       id: uuidv4(),
+  //       identification: this.formRolUser.value.identification,
+  //       identificationTypeId: 1,
+  //       fullName: this.formRolUser.value.fullName,
+  //       avatarUrl: this.formRolUser.value.avatarUrl,
+  //       username: this.formRolUser.value.username,
+  //       phone: this.formRolUser.value.phone,
+  //       email: this.formRolUser.value.email,
+  //       password: this.formRolUser.value.password,
+  //       passwordConfirmation: this.formRolUser.get('confirmPassword')?.value,
+  //       roleId: 1
+  //     };
+  //     // this._userRoleService.rolUser(userToRegister).subscribe({
+  //     //   next: () => {
+  //     //     this._router.navigate(['/home']);
+  //     //   }
+  //     // });
+  //   } else {
+  //     // Muestra errores si uno de los formularios no es válido
+  //   }
+  // }
 
   /**
    * Ver contraseña en escribir contraseña.
